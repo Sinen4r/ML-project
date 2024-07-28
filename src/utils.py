c4c6ee53f663b49d8222be4a0a015f39ee0af652
linasmiri <153398440+Sinen4r@users.noreply.github.com>
2024-07-28 21:24:35 +0100

    adding hyperParameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 from src.exception import CustomerException
 def save_object(file_path,obj):
@@ -18,13 +20,18 @@ def save_object(file_path,obj):
         raise CustomerException(e,sys)
 
 
-def evaluate_model(xTrain,yTrain,xTest,yTest,models):
+def evaluate_model(xTrain,yTrain,xTest,yTest,models,param):
     
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            g=GridSearchCV(model,para,cv=3)
+            g.fit(xTrain,yTrain)
+            model.set_params(**g.best_params_)
             model.fit(xTrain,yTrain)
+            
             yTrain_pred=model.predict(xTrain)   
             yTest_pred=model.predict(xTest)
 
